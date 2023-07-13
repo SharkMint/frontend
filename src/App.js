10f3596785e83b8e8b9194d1788ea4a60d3fa05a
Maicol Jaquez <maicol120205@gmail.com>
2023-07-13 02:41:22 +0100

    Frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import Table from "react-bootstrap/Table";
+import { Container } from "react-bootstrap";
 
 function App() {
+  const [data, setData] = useState();
+
+  // https://www.bitmex.com/api/v1
+
+  const baseUrl = "https://localhost:44303/api/Announcement";
+
+  const getDataApi = () => {
+    const response = fetch(baseUrl, {
+      method: "GET",
+      withCredentials: true,
+      crossorigin: true,
+      mode: "no-cors",
+    })
+      .then((response) => response.json())
+      .then((res) => res.json())
+      .then((data) => {
+        setData(response);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  useEffect(() => {
+    getDataApi();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3 className="tittle-header">Hello DevTest</h3>
       </header>
+
+      <div className="body">
+        <Container>
+          <Table striped bordered hover size="md" >
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>Link</th>
+                <th>Title</th>
+                <th>Content</th>
+                <th>Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data > 0 &&
+                data.map((item, i) => {
+                  <tr>
+                    <td key={i}>{item.Id}</td>
+                    <td key={i}>{item.Link}</td>
+                    <td key={i}>{item.Title}</td>
+                    <td key={i}>{item.Content}</td>
+                    <td key={i}>{item.Date}</td>
+                  </tr>;
+                })}
+            </tbody>
+          </Table>
+        </Container>
+      </div>
     </div>
   );
 }
